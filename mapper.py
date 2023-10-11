5a9efdeb4588d18b658ddd15f93534e68aaa7030
Макарова Юлия <70716673+MakarovaJulia@users.noreply.github.com>
2023-10-11 18:55:24 +0300

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
+
 import sys
 
 for line in sys.stdin:
     line = line.strip()
-    columns = line.split(",")
-    if len(columns) == 28: 
-        try:
-            countReviews = int(columns[16])
-            print ("%s\n%s" % (columns[3],countReviews))
-        except ValueError:
-            pass
+    data = line.split(",")
+    try:
+        print("%s\t%s" % (data[2], 1))
+    except Exception as e:
+        continue
